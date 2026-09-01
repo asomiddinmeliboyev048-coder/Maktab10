c2f4d2e111f3a6734ed5710d2ec39af75d25918b
asomiddinmeliboyev048-coder <asomiddinmeliboyev048@gmail.com>
2026-09-01 11:55:15 +0500

    Update user route to use UserController

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,4 @@ use App\Http\Controllers\TelegramController;
 
 Route::post('/telegram/webhook', [TelegramController::class, 'handleWebhook']);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::middleware('auth:api')->get('/user', 'Auth\UserController@current');
